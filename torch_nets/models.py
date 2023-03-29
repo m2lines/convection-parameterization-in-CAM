cf8e99dad15d92b8c8e72c6cb38444183982e0c3
jdenholm <j.denholm.2017@gmail.com>
2023-03-29 10:45:12 +0100

    :recyle: Updated imports

--- torch_nets/models.py
+++ torch_nets/models.py
@@ -1,14 +1,17 @@
 """Neural network architectures."""
 
+from torch.nn import Module, Linear, Dropout, BatchNorm1d
+from torch.nn import functional as F
 
-class Net_ANN_no_BN(nn.Module):
+
+class Net_ANN_no_BN(Module):
     def __init__(self, n_in, n_out, neurons=128, dropoff=0.0):
         super(Net_ANN_no_BN, self).__init__()
-        self.linear1 = nn.Linear(n_in, neurons)
-        self.linear2 = nn.Linear(neurons, n_out)
-        self.lin_drop = nn.Dropout(
+        self.linear1 = Linear(n_in, neurons)
+        self.linear2 = Linear(neurons, n_out)
+        self.lin_drop = Dropout(
             dropoff
-        )  # regularization method to prevent overfitting.
+        )  # regularization method to prevent overfitt1ing.
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
@@ -17,26 +20,26 @@ class Net_ANN_no_BN(nn.Module):
         return x
 
 
-class Net_ANN_5(nn.Module):
+class Net_ANN_5(Module):
     def __init__(self, n_in, n_out, dropoff=0.0):
         super(Net_ANN_5, self).__init__()
-        self.linear1 = nn.Linear(n_in, 256)
-        self.linear2 = nn.Linear(256, 256)
-        self.linear3 = nn.Linear(256, 256)
-        self.linear4 = nn.Linear(256, 256)
-        self.linear5 = nn.Linear(256, n_out)
+        self.linear1 = Linear(n_in, 256)
+        self.linear2 = Linear(256, 256)
+        self.linear3 = Linear(256, 256)
+        self.linear4 = Linear(256, 256)
+        self.linear5 = Linear(256, n_out)
 
-        self.dense1_bn = nn.BatchNorm1d(256)
-        self.dense2_bn = nn.BatchNorm1d(256)
-        self.dense3_bn = nn.BatchNorm1d(256)
-        self.dense4_bn = nn.BatchNorm1d(256)
+        self.dense1_bn = BatchNorm1d(256)
+        self.dense2_bn = BatchNorm1d(256)
+        self.dense3_bn = BatchNorm1d(256)
+        self.dense4_bn = BatchNorm1d(256)
 
-        self.lin_drop = nn.Dropout(
+        self.lin_drop = Dropout(
             dropoff
         )  # regularization method to prevent overfitting.
-        self.lin_drop2 = nn.Dropout(dropoff)
-        self.lin_drop3 = nn.Dropout(dropoff)
-        self.lin_drop4 = nn.Dropout(dropoff)
+        self.lin_drop2 = Dropout(dropoff)
+        self.lin_drop3 = Dropout(dropoff)
+        self.lin_drop4 = Dropout(dropoff)
 
     def forward(self, x):
         x = self.dense1_bn(F.relu(self.linear1(x)))
@@ -51,17 +54,17 @@ class Net_ANN_5(nn.Module):
         return x
 
 
-class Net_ANN_3_no_BN(nn.Module):
+class Net_ANN_3_no_BN(Module):
     def __init__(self, n_in, n_out, neurons=128, dropoff=0.0):
         super(Net_ANN_3_no_BN, self).__init__()
-        self.linear1 = nn.Linear(n_in, neurons)
-        self.linear2 = nn.Linear(neurons, neurons)
-        self.linear3 = nn.Linear(neurons, n_out)
+        self.linear1 = Linear(n_in, neurons)
+        self.linear2 = Linear(neurons, neurons)
+        self.linear3 = Linear(neurons, n_out)
 
-        self.lin_drop = nn.Dropout(
+        self.lin_drop = Dropout(
             dropoff
         )  # regularization method to prevent overfitting.
-        self.lin_drop2 = nn.Dropout(dropoff)
+        self.lin_drop2 = Dropout(dropoff)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
@@ -72,19 +75,19 @@ class Net_ANN_3_no_BN(nn.Module):
         return x
 
 
-class Net_ANN_4_no_BN(nn.Module):
+class Net_ANN_4_no_BN(Module):
     def __init__(self, n_in, n_out, neurons=128, dropoff=0.0):
         super(Net_ANN_4_no_BN, self).__init__()
-        self.linear1 = nn.Linear(n_in, neurons)
-        self.linear2 = nn.Linear(neurons, neurons)
-        self.linear3 = nn.Linear(neurons, neurons)
-        self.linear4 = nn.Linear(neurons, n_out)
+        self.linear1 = Linear(n_in, neurons)
+        self.linear2 = Linear(neurons, neurons)
+        self.linear3 = Linear(neurons, neurons)
+        self.linear4 = Linear(neurons, n_out)
 
-        self.lin_drop = nn.Dropout(
+        self.lin_drop = Dropout(
             dropoff
         )  # regularization method to prevent overfitting.
-        self.lin_drop2 = nn.Dropout(dropoff)
-        self.lin_drop3 = nn.Dropout(dropoff)
+        self.lin_drop2 = Dropout(dropoff)
+        self.lin_drop3 = Dropout(dropoff)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
@@ -97,21 +100,21 @@ class Net_ANN_4_no_BN(nn.Module):
         return x
 
 
-class Net_ANN_5_no_BN(nn.Module):
+class Net_ANN_5_no_BN(Module):
     def __init__(self, n_in, n_out, neurons=128, dropoff=0.0):
         super(Net_ANN_5_no_BN, self).__init__()
-        self.linear1 = nn.Linear(n_in, neurons)
-        self.linear2 = nn.Linear(neurons, neurons)
-        self.linear3 = nn.Linear(neurons, neurons)
-        self.linear4 = nn.Linear(neurons, neurons)
-        self.linear5 = nn.Linear(neurons, n_out)
+        self.linear1 = Linear(n_in, neurons)
+        self.linear2 = Linear(neurons, neurons)
+        self.linear3 = Linear(neurons, neurons)
+        self.linear4 = Linear(neurons, neurons)
+        self.linear5 = Linear(neurons, n_out)
 
-        self.lin_drop = nn.Dropout(
+        self.lin_drop = Dropout(
             dropoff
         )  # regularization method to prevent overfitting.
-        self.lin_drop2 = nn.Dropout(dropoff)
-        self.lin_drop3 = nn.Dropout(dropoff)
-        self.lin_drop4 = nn.Dropout(dropoff)
+        self.lin_drop2 = Dropout(dropoff)
+        self.lin_drop3 = Dropout(dropoff)
+        self.lin_drop4 = Dropout(dropoff)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
@@ -126,23 +129,23 @@ class Net_ANN_5_no_BN(nn.Module):
         return x
 
 
-class Net_ANN_6_no_BN(nn.Module):
+class Net_ANN_6_no_BN(Module):
     def __init__(self, n_in, n_out, neurons=128, dropoff=0.0):
         super(Net_ANN_6_no_BN, self).__init__()
-        self.linear1 = nn.Linear(n_in, neurons)
-        self.linear2 = nn.Linear(neurons, neurons)
-        self.linear3 = nn.Linear(neurons, neurons)
-        self.linear4 = nn.Linear(neurons, neurons)
-        self.linear5 = nn.Linear(neurons, neurons)
-        self.linear6 = nn.Linear(neurons, n_out)
+        self.linear1 = Linear(n_in, neurons)
+        self.linear2 = Linear(neurons, neurons)
+        self.linear3 = Linear(neurons, neurons)
+        self.linear4 = Linear(neurons, neurons)
+        self.linear5 = Linear(neurons, neurons)
+        self.linear6 = Linear(neurons, n_out)
 
-        self.lin_drop = nn.Dropout(
+        self.lin_drop = Dropout(
             dropoff
         )  # regularization method to prevent overfitting.
-        self.lin_drop2 = nn.Dropout(dropoff)
-        self.lin_drop3 = nn.Dropout(dropoff)
-        self.lin_drop4 = nn.Dropout(dropoff)
-        self.lin_drop5 = nn.Dropout(dropoff)
+        self.lin_drop2 = Dropout(dropoff)
+        self.lin_drop3 = Dropout(dropoff)
+        self.lin_drop4 = Dropout(dropoff)
+        self.lin_drop5 = Dropout(dropoff)
 
     def forward(self, x):
         x = F.relu(self.linear1(x))
