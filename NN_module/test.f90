e09d7907b554065ff75ad365cdf2bee0c6fb7b3e
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-04-13 13:22:41 +0100

    Add routines to test relu (more to come)

--- NN_module/test.f90
+++ NN_module/test.f90
@@ -1,5 +1,5 @@
 program run_tests
-    use nn_convection_flux_mod, only: relu
+    use nn_convection_flux_mod, only: relu, nn_convection_flux_init
 
     implicit none
 
@@ -9,5 +9,6 @@ program run_tests
 
     write (*,*) test_array
 
+    call nn_convection_flux_init("/home/jwa34/convection-parameterization-in-CAM/torch_nets/qobsTTFFFFFTF30FFTFTF30TTFTFTFFF80FFTFTTF2699FFFF_X01_no_qp_no_adv_surf_F_Tin_qin_disteq_O_Trad_rest_Tadv_qadv_qout_qsed_RESCALED_7epochs_no_drop_REAL_NN_layers5in61out148_BN_F_te70.nc")
 
 end program run_tests
