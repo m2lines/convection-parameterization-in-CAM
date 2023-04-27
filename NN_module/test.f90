7aa5ef468366e13f9799cfa66bd7c6d5feb684ba
dorchard <dom.orchard@gmail.com>
2023-04-27 18:29:05 +0200

    finalize in smoke test

--- NN_module/test.f90
+++ NN_module/test.f90
@@ -1,5 +1,5 @@
 program run_tests
-    use nn_convection_flux_mod, only: relu, nn_convection_flux_init
+    use nn_convection_flux_mod, only: relu, nn_convection_flux_init, nn_convection_flux_finalize
 
     implicit none
 
@@ -10,6 +10,7 @@ program run_tests
 
     write (*,*) test_array
 
-    call nn_convection_flux_init("/Users/jwa34/Documents/code/convection-parameterization-in-CAM/torch_nets/qobsTTFFFFFTF30FFTFTF30TTFTFTFFF80FFTFTTF2699FFFF_X01_no_qp_no_adv_surf_F_Tin_qin_disteq_O_Trad_rest_Tadv_qadv_qout_qsed_RESCALED_7epochs_no_drop_REAL_NN_layers5in61out148_BN_F_te70.nc", nin, nout)
+    call nn_convection_flux_init("../torch_nets/qobsTTFFFFFTF30FFTFTF30TTFTFTFFF80FFTFTTF2699FFFF_X01_no_qp_no_adv_surf_F_Tin_qin_disteq_O_Trad_rest_Tadv_qadv_qout_qsed_RESCALED_7epochs_no_drop_REAL_NN_layers5in61out148_BN_F_te70.nc", nin, nout)
+    call nn_convection_flux_finalize()
 
 end program run_tests
