b73145a5d118b7f40f501c99aa096b74786dc357
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-09-12 14:31:17 +0100

    Add generic interpolation routine for inputs (CAM grid to SAM grid).

--- NN_module/test_cam_interface.f90
+++ NN_module/test_cam_interface.f90
@@ -8,9 +8,24 @@ program run_cam_tests
 
     implicit none
 
+    real, dimension(4, 3) :: p_cam
+    real, dimension(4, 3) :: var_cam
+    real, dimension(4) :: ps_cam
+    real, dimension(4, 30) :: var_sam
+
+    ! Test interpolation by interpolating pressure to pressure => should match pres from SAM
+    p_cam = reshape((/ 1000.0, 1000.0, 1000.0, 1000.0, 500.0, 500.0, 500.0, 500.0, 10.0, 10.0, 10.0, 10.0 /), (/ 4, 3 /))
+    ps_cam = (/ 1000.0, 1000.0, 1000.0, 1000.0/)
+    var_cam = reshape((/ 1000.0, 1000.0, 1000.0, 1000.0, 500.0, 500.0, 500.0, 500.0, 10.0, 10.0, 10.0, 10.0 /), (/ 4, 3 /))
+    var_sam = -1.0
+
     call nn_convection_flux_CAM_init(trim("NN_weights_YOG_convection.nc"), trim("resources/SAM_sounding.nc"))
-    call interp_to_sam(reshape((/ 1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0, 4.0 /), (/ 4, 3, 1 /)))
+    call interp_to_sam(p_cam, ps_cam, var_cam, var_sam)
     call nn_convection_flux_CAM_finalize()
+    do k=1,30
+        write(*,*) var_sam(2, k)
+    end do
+
 
 
 
