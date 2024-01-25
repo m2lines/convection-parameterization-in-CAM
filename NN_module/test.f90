ce3e53cc9e780e84f1bed1ad5fdd034cee82de1d
jatkinson1000 <jwa34@cam.ac.uk>
2024-01-25 09:15:52 +0000

    Add testing framework for Fortran NN routines.

--- NN_module/test.f90
+++ NN_module/test.f90
@@ -1,146 +1,122 @@
-program run_tests
-    use nn_cf_net_mod, only: relu, nn_cf_net_init, nn_cf_net_finalize
+module tests
+  !! Module containing individual tests for the CAM ML code
 
-    use nn_convection_flux_mod, only: nn_convection_flux, nn_convection_flux_init, nn_convection_flux_finalize
+  !--------------------------------------------------------------------------
+  ! Libraries to use
+  use netcdf
+  use nn_cf_net_mod, only: relu, net_forward, nn_cf_net_init, nn_cf_net_finalize
+  use test_utils, only: assert_array_equal
 
-    implicit none
+  implicit none
 
-    real(4), dimension(4) :: test_array = (/ -1.0, 0.0, 0.5, 1.0 /)
-    integer :: nin, nout
+  character(len=15) :: pass = char(27)//'[32m'//'PASSED'//char(27)//'[0m'
+  character(len=15) :: fail = char(27)//'[31m'//'FAILED'//char(27)//'[0m'
 
+  real(4), dimension(148) :: nn_out_ones
 
+  contains
 
+    subroutine test_relu(test_name)
+      !! Test relu function is working as expected
 
-    ! Parameters from SAM that are used here
-    ! From domain.f90
-    integer, parameter :: YES3D = 1
-        !! Domain dimensionality: 1 - 3D, 0 - 2D
-    integer, parameter :: nx_gl = 1
-        !! Number of grid points in X - Yani changed to 36 from 32
-    integer, parameter :: ny_gl = 1
-        !! Number of grid points in Y
-    integer, parameter :: nz_gl = 48
-        !! Number of pressure (scalar) levels
-    integer, parameter :: nsubdomains_x  = 1
-        !! No of subdomains in x
-    integer, parameter :: nsubdomains_y  = 1
-        !! No of subdomains in y
+      character(len=*), intent(in) :: test_name
+      real(4), dimension(4) :: test_array = (/ -1.0, 0.0, 0.5, 1.0 /)
+      real(4), dimension(4) :: res_array = (/ 0.0, 0.0, 0.5, 1.0 /)
 
-    ! From grid.f90
-    integer, parameter :: nx = nx_gl/nsubdomains_x
-        !! Number of x points in a subdomain
-    integer, parameter :: ny = ny_gl/nsubdomains_y
-        !! Number of y points in a subdomain
-    integer, parameter :: nz = nz_gl+1
-        !! Number of z points in a subdomain
-    ! Store useful variations on these values
-    integer, parameter :: nzm = nz-1
-    integer, parameter :: nxp3 = nx + 3
-    integer, parameter :: nyp3 = ny + 3 * YES3D
-    integer, parameter :: dimx1_s = -2
-    integer, parameter :: dimx2_s = nxp3
-    integer, parameter :: dimy1_s = 1-3*YES3D
-    integer, parameter :: dimy2_s = nyp3
+      call relu(test_array)
 
-    integer :: nrf
+      call assert_array_equal(res_array, test_array, test_name)
 
-    != unit J :: t
-    real t(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm)
-        !! moist static energy
-    real q(dimx1_s:dimx2_s, dimy1_s:dimy2_s, nzm)
-        !! total water
-    ! fluxes at the top and bottom of the domain:
-    real precsfc(nx,ny)
-        !! surface precip. rate
-    !  Horizontally varying stuff (as a function of xy)
-    real prec_xy(nx,ny)
-        !! surface precipitation rate
-    ! reference vertical profiles:
+    end subroutine test_relu
 
-    != unit (kg / m**3) :: rho
-    real rho(nzm)
-        !! air density at pressure levels
+    subroutine test_nn_cf_init(test_name)
+      !! Test NN initialisation is working as expected
+      !! Checks that nin and nout are read in as expected
+      !! Checking deeper requires interaction with nn_cf_net_mod module vars
 
-    real :: tabs(nx, ny, nzm)
-        !! absolute temperature
+      character(len=*), intent(in) :: test_name
+      character(len=1024) :: nn_filename
+      integer :: nin, nout
 
-    ! Input Variables
+      nn_filename = "./NN_weights_YOG_convection.nc"
 
-    ! Fields from beginning of time step used as NN inputs
-    real tabs_i(nx,ny,nzm)
-        !! Temperature
-    real q_i(nx,ny,nzm)
-        !! Non-precipitating water mixing ratio
-    real :: y_in(nx,ny)
-        !! Distance of column from equator (proxy for insolation and sfc albedo)
-    real adz(nzm)
-        !! ratio of the grid spacing to dz for pressure levels
-    != unit s :: dtn
-    real dtn
-        !! current dynamical timestep (can be smaller than dt)
-    real dz
-        !! current dynamical timestep (can be smaller than dt)
+      call nn_cf_net_init(nn_filename, nin, nout, 30)
 
+      call nn_cf_net_finalize()
 
-    real, dimension(nx,ny,30) :: t_rad_rest_tend, &
-                                 t_delta_adv, q_delta_adv, &
-                                 t_delta_auto, q_delta_auto, &
-                                 t_delta_sed, q_delta_sed
-    real, dimension(nx,ny) :: prec_sed
-
-    ! Test NN routines
-
-    call relu(test_array)
-
-    write (*,*) test_array
-
-    call nn_cf_net_init("./NN_weights_YOG_convection.nc", nin, nout, 30)
-    call nn_cf_net_finalize()
-
-
-    ! Test convection flux routines
-
-
-    t = 0.
-    q = 0.4
-    precsfc = 0.
-    prec_xy = 0.
-    rho = 1.
-    tabs = 1.
-    tabs_i = 287.15
-    q_i = 0.2
-    adz = 1.
-    y_in = 1.
-    dz = 1.
-    dtn = 1.
-
-    nrf = 30
-
-    call nn_convection_flux_init("./NN_weights_YOG_convection.nc")
-
-    call nn_convection_flux(tabs_i, q_i, y_in, &
-                            tabs, &
-                            t, q, &
-                            rho, adz, dz, dtn, &
-                            t_rad_rest_tend, &
-                            t_delta_adv, q_delta_adv, &
-                            t_delta_auto, q_delta_auto, &
-                            t_delta_sed, q_delta_sed, prec_sed)
-
-    q(:,:,1:nrf) = q(:,:,1:nrf) + q_delta_adv(:,:,:) &
-                                + q_delta_auto(:,:,:) &
-                                + q_delta_sed(:,:,:)
-    t(:,:,1:nrf) = t(:,:,1:nrf) + t_delta_adv(:,:,:) &
-                                + t_delta_auto(:,:,:) &
-                                + t_delta_sed(:,:,:) &
-                                + t_rad_rest_tend(:,:,:)*dtn
-
-    call nn_convection_flux_finalize()
-
+      if (nin == 61) then
+        write(*, '(A, " :: [", A, " - nin]")') pass, trim(test_name)
+        if (nout == 148) then
+          write(*, '(A, " :: [", A, " - nout]")') pass, trim(test_name)
+        else
+          write(*, '(A, " :: [", A, "] with nout = ", I3)') fail, trim(test_name), nout
+        end if
+      else
+        write(*, '(A, " :: [", A, "] with nin = ", I3)') fail, trim(test_name), nin
+      end if
     
-    write (*,*) t(-2:2, 0, 1)
-    write (*,*) t(0, -2:2, 1)
-    write (*,*) t(-2, -2, 1:48)
-    ! write (*,*) t(-1, -1, 1:48)
+    end subroutine test_nn_cf_init
+
+    subroutine test_nn(test_name)
+      !! Test NN is producing the expected results.
+
+      integer :: i, nin, nout
+      character(len=*), intent(in) :: test_name
+      character(len=1024) :: nn_filename
+      real(4) :: nn_in(61)
+      real(4) :: nn_out(148)
+
+      nn_in = 1.0
+
+      nn_filename = "./NN_weights_YOG_convection.nc"
+      call nn_cf_net_init(nn_filename, nin, nout, 30)
+
+      call net_forward(nn_in, nn_out)
+
+      call nn_cf_net_finalize()
+
+      call assert_array_equal(nn_out, nn_out_ones, test_name, 1.0e-4)
+
+    end subroutine test_nn
+
+    subroutine load_nn_out_ones(nn_ones_file)
+      !! Load the result of running the NN with ones
+
+      integer :: io, stat, i
+      character(len=512) :: msg
+      character(len=*) :: nn_ones_file
+
+      open(newunit=io, file=trim(nn_ones_file), status="old", action="read", &
+           iostat=stat, iomsg=msg)
+      if (stat /= 0) then
+        print *, trim(msg)
+        stop
+      end if
+      do i = 1,148
+        read(io, *) nn_out_ones(i)
+      enddo
+      close(io)
+
+    end subroutine load_nn_out_ones
+
+end module tests
+
+
+
+program run_tests
+
+  use tests
+
+  implicit none
+
+  character(len=1024) :: nn_filename
+
+  ! Test NN routines
+
+  call test_relu("test_relu")
+  call test_nn_cf_init("test_nn_cf_init")
+
+  call load_nn_out_ones("nn_ones.txt")
+  call test_nn("Test NN ones")
 
 end program run_tests
