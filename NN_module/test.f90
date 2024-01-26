79238129aeb8d2f47f7d74ab4895be3b5e1fb2ad
jatkinson1000 <jwa34@cam.ac.uk>
2024-01-26 10:20:46 +0000

    Add test for full parameterisation.

--- NN_module/test.f90
+++ NN_module/test.f90
@@ -5,6 +5,7 @@ module tests
   ! Libraries to use
   use netcdf
   use nn_cf_net_mod, only: relu, net_forward, nn_cf_net_init, nn_cf_net_finalize
+  use nn_convection_flux_mod, only:   nn_convection_flux, nn_convection_flux_init, nn_convection_flux_finalize
   use test_utils, only: assert_array_equal
 
   implicit none
@@ -79,6 +80,94 @@ module tests
 
     end subroutine test_nn
 
+    subroutine test_param(test_name)
+      !! Test Parameterisation is producing the same results as it initially did.
+      !! Run for a single column with physically plausible parameters.
+
+      integer :: i, io, stat
+      character(len=*), intent(in) :: test_name
+      character(len=1024) :: nn_filename
+      character(len=512) :: msg
+
+      real :: tabs_i(1, 1, 48) = 293.15
+      real :: q_i(1, 1, 48) = 0.5
+      real :: y_in(1, 48) = 0.0
+      real :: tabs(1, 1, 48) = 293.15
+      real :: t_0(1, 1, 48) =  1.0e4
+      real :: q_0(1, 1, 48) = 0.5
+      real :: rho(48) = 1.2
+      real :: adz(48) = 1.0
+      real :: dz = 100.0
+      real :: dtn = 2.0
+      real, dimension(1,1,30) :: t_delta_adv, q_delta_adv, &
+                                t_delta_auto, q_delta_auto, &
+                                t_delta_sed, q_delta_sed
+      real :: t_rad_rest_tend(1,1,30)
+      real :: prec_sed(1,1)
+
+      real, dimension(1,1,30) :: t_delta_adv_dat, q_delta_adv_dat, &
+                                t_delta_auto_dat, q_delta_auto_dat, &
+                                t_delta_sed_dat, q_delta_sed_dat
+      real :: t_rad_rest_tend_dat(1,1,30)
+      real :: prec_sed_dat(1,1)
+      
+      nn_filename = "./NN_weights_YOG_convection.nc"
+      call nn_convection_flux_init(nn_filename)
+
+      call nn_convection_flux(tabs_i, q_i, y_in, &
+                                  tabs, &
+                                  t_0, q_0, &
+                                  rho, adz, dz, dtn, &
+                                  t_rad_rest_tend, &
+                                  t_delta_adv, q_delta_adv, &
+                                  t_delta_auto, q_delta_auto, &
+                                  t_delta_sed, q_delta_sed, prec_sed)
+
+      call nn_convection_flux_finalize()
+
+      nn_filename = "param_test.txt"
+
+      ! Writing data out to file from original code runniing
+      ! open(newunit=io, file=trim(nn_filename), status="replace", action="write", &
+      !      iostat=stat, iomsg=msg)
+      ! if (stat /= 0) then
+      !   print *, trim(msg)
+      !   stop
+      ! end if
+      ! do i = 1,30
+      !   write(io, '(7E18.8)') t_delta_adv(1,1,i), q_delta_adv(1,1,i), &
+      !                t_delta_auto(1,1,i), q_delta_auto(1,1,i), &
+      !                t_delta_sed(1,1,i), q_delta_sed(1,1,i), t_rad_rest_tend(1,1,i)
+      ! enddo
+      ! write(io, '(E18.8)') prec_sed(1,1)
+      ! close(io)
+
+      open(newunit=io, file=trim(nn_filename), status="old", action="read", &
+           iostat=stat, iomsg=msg)
+      if (stat /= 0) then
+        print *, trim(msg)
+        stop
+      end if
+      do i = 1,30
+        read(io, '(7E18.8)') t_delta_adv_dat(1,1,i), q_delta_adv_dat(1,1,i), &
+                     t_delta_auto_dat(1,1,i), q_delta_auto_dat(1,1,i), &
+                     t_delta_sed_dat(1,1,i), q_delta_sed_dat(1,1,i), &
+                     t_rad_rest_tend_dat(1,1,i)
+      enddo
+      read(io, '(E18.8)') prec_sed_dat(1,1)
+      close(io)
+
+      call assert_array_equal(t_delta_adv, t_delta_adv_dat, test_name//" t adv", 1.0e-6)
+      call assert_array_equal(q_delta_adv, q_delta_adv_dat, test_name//" q adv", 1.0e-6)
+      call assert_array_equal(t_delta_auto, t_delta_auto_dat, test_name//" t auto", 1.0e-6)
+      call assert_array_equal(q_delta_auto, q_delta_auto_dat, test_name//" q auto", 1.0e-6)
+      call assert_array_equal(t_delta_sed, t_delta_sed_dat, test_name//" t sed", 1.0e-6)
+      call assert_array_equal(q_delta_sed, q_delta_sed_dat, test_name//" q sed", 1.0e-6)
+      call assert_array_equal(t_rad_rest_tend, t_rad_rest_tend_dat, test_name//" t rad", 1.0e-6)
+      call assert_array_equal(prec_sed, prec_sed_dat, test_name//" prec", 1.0e-6)
+
+    end subroutine test_param
+
     subroutine load_nn_out_ones(nn_ones_file)
       !! Load the result of running the NN with ones
 
@@ -118,5 +207,7 @@ program run_tests
 
   call load_nn_out_ones("nn_ones.txt")
   call test_nn("Test NN ones")
+
+  call test_param("Test param simple")
 
 end program run_tests
