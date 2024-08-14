399612b1f5eb6cdbb5c20765907965f0ffdea33f
Jack Atkinson <jwa34@cam.ac.uk>
2024-08-14 14:15:33 +0100

    Update parametersation files to match those currently used in the CAM implementation.
    SAM_consts, nn_cf_net, nn_convection_flux, nn_interface_CAM.
    
    This involves:
    * moving to specify the precision as real(8) (as used in CAM)
    * better treatment of precipitation units (to match CAM)
    * addition of surface interpolation scheme
    * removal of subcycling components of the parameterisation
    *

--- NN_module/SAM_consts.f90
+++ NN_module/SAM_consts.f90
@@ -1,6 +1,8 @@
 module SAM_consts_mod
   !! Physical constants from the SAM Model required for variable conversions
   
+  use netcdf
+
   implicit none
   public
 
@@ -12,76 +14,76 @@ module SAM_consts_mod
   ! Physical Constants:
 
   != unit (m / s^2)
-  real, parameter :: ggr = 9.81
+  real(8), parameter :: ggr = 9.81
       !! Gravity acceleration, m/s2
 
   != unit J / kg :: lcond, lsub, lfus
-  real, parameter :: lfus = 0.3336e+06
+  real(8), parameter :: lfus = 0.3336e+06
       !! Latent heat of fusion
-  real, parameter :: lcond = 2.5104e+06
+  real(8), parameter :: lcond = 2.5104e+06
       !! Latent heat of condensation
-  real, parameter :: lsub = 2.8440e+06
+  real(8), parameter :: lsub = 2.8440e+06
       !! Latent heat of sublimation, J/kg
 
   != unit (J / kg) / K :: cp
-  real, parameter :: cp = 1004.
+  real(8), parameter :: cp = 1004.
       !! Specific heat of air
   
   ! unit K :: fac_cond, fac_fus, fac_sub
-  real, parameter :: fac_cond = lcond/cp
+  real(8), parameter :: fac_cond = lcond/cp
       !!
-  real, parameter :: fac_fus = lfus/cp
+  real(8), parameter :: fac_fus = lfus/cp
       !!
-  real, parameter :: fac_sub = lsub/cp
+  real(8), parameter :: fac_sub = lsub/cp
       !!
 
   ! Temperatures limits for various hydrometeors
   != unit K :: tprmin
-  real, parameter :: tprmin = 268.16
+  real(8), parameter :: tprmin = 268.16
       !! Minimum temperature for rain
   
   != unit K :: tprmax
-  real, parameter :: tprmax = 283.16
+  real(8), parameter :: tprmax = 283.16
       !! Maximum temperature for snow+graupel, K
   
   != unit K :: tbgmin
-  real, parameter :: tbgmin = 253.16
+  real(8), parameter :: tbgmin = 253.16
       !! Minimum temperature for cloud water.
   
   != unit K :: tbgmin
-  real, parameter :: tbgmax = 273.16
+  real(8), parameter :: tbgmax = 273.16
       !! Maximum temperature for cloud ice, K
 
   != unit K :: tbgmin
-  real, parameter :: tgrmin = 223.16
+  real(8), parameter :: tgrmin = 223.16
       !! Maximum temperature for snow, K
 
   != unit K :: tbgmin
-  real, parameter :: tgrmax = 283.16
+  real(8), parameter :: tgrmax = 283.16
       !! Maximum temperature for graupel, K
 
   ! Misc. microphysics variables
   ! != unit 1 / K :: a_pr
-  real, parameter :: a_pr = 1./(tprmax-tprmin)
+  real(8), parameter :: a_pr = 1./(tprmax-tprmin)
       !! Misc. microphysics variables
   
   != unit 1 / K :: a_bg
-  real, parameter :: a_bg = 1./(tbgmax-tbgmin)
+  real(8), parameter :: a_bg = 1./(tbgmax-tbgmin)
       !! Misc. microphysics variables
 
-  real, parameter :: an = 1./(tbgmax-tbgmin)
-  real, parameter :: bn = tbgmin * an
-  real, parameter :: ap = 1./(tprmax-tprmin)
-  real, parameter :: bp = tprmin * ap
-  real, parameter :: fac1 = fac_cond+(1+bp)*fac_fus
-  real, parameter :: fac2 = fac_fus*ap
-  real, parameter :: ag = 1./(tgrmax-tgrmin)
+  real(8), parameter :: an = 1./(tbgmax-tbgmin)
+  real(8), parameter :: bn = tbgmin * an
+  real(8), parameter :: ap = 1./(tprmax-tprmin)
+  real(8), parameter :: bp = tprmin * ap
+  real(8), parameter :: fac1 = fac_cond+(1+bp)*fac_fus
+  real(8), parameter :: fac2 = fac_fus*ap
+  real(8), parameter :: ag = 1./(tgrmax-tgrmin)
 
   ! ---------------------------
   ! SAM Grid Variables
   ! ---------------------------
-  integer, parameter :: input_ver_dim = 48
-      !! Set to 48 in setparm.f90 of SAM. Same as nz_gl??
+  integer, parameter :: input_ver_dim = 30
+      !! The number of cells in a SAM atmospheric column on which the neural net was trained
   
   ! Outputs from NN are supplied at lowest 30 half-model levels for sedimentation fluxes,
   ! and at 29 levels for fluxes (as flux at bottom boundary is zero).
@@ -90,6 +92,8 @@ module SAM_consts_mod
   integer, parameter :: nrfq = nrf - 1
       !! number of vertical levels the NN uses when boundary condition is set to 0
 
+  real(8), parameter :: dt_sam = 24.0
+      !! SAM timestep in seconds
 
 !---------------------------------------------------------------------
 ! Functions and Subroutines
@@ -97,9 +101,9 @@ module SAM_consts_mod
 contains
 
   != unit 1 :: omegan
-  real function omegan(tabs)
+  real(8) function omegan(tabs)
       != unit K :: tabs
-      real, intent(in) :: tabs
+      real(8), intent(in) :: tabs
           !! Absolute temperature
 
       omegan = max(0., min(1., (tabs-tbgmin)*a_bg))
@@ -112,8 +116,6 @@ contains
   subroutine check(err_status)
   !! Check error status after netcdf call and print message for
   !! error codes.
-
-  use netcdf
 
   integer, intent(in) :: err_status
     !! error status from nf90 function
